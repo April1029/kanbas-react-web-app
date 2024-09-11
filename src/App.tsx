9ae6fb850144a021bc13c5a9b2fa94117ffce786
April1029 <jj5rb@virginia.edu>
2024-09-10 19:51:35 -0700

    a1 lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Labs from './Labs';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>Welcome to Web Dev!</h1>
+      < Labs />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to Web Dev!
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
